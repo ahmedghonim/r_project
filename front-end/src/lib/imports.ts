6ea170bf549e3f6a8901324af55247491d19b2d9
MohamedMamdoh144 <mmamdoh144@gmail.com>
2024-07-12 18:05:25 +0300

    bug fixed

--- front-end/src/lib/imports.ts
+++ front-end/src/lib/imports.ts
@@ -5,7 +5,7 @@ import fsPromises from "fs/promises"
 export async function getPresets() {
     const filePath = path.join(process.cwd(), 'Presets.json');
     const jsonData = await fsPromises.readFile(filePath);
-    const objectData = await JSON.parse(jsonData);
+    const objectData = await JSON.parse(jsonData.toString());
   
     return {
        objectData
